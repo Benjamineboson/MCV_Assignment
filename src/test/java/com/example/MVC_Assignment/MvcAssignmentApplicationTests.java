fc62299b66bf02c5219e79be1a4845b0d9bb323c
Benjamin Boson <benjamineboson@gmail.com>
2020-03-22 18:28:39 +0100

    init commit

---
+++ src/test/java/com/example/MVC_Assignment/MvcAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MVC_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
